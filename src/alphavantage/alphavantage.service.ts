a418991750642c7e31da81dafebf6f91a6bfda99
lbuckalew <louis.buckalew@gmail.com>
2025-11-20 18:04:56 -0600

    more typing!

--- src/alphavantage/alphavantage.service.ts
+++ src/alphavantage/alphavantage.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Holding, Sector, Etf} from '../intersections/intersections.service'
 
 export interface AlphavantageEtfPayload {
   net_assets: string;
-  net_expense_ration: string;
+  net_expense_ratio: string;
   portfolio_turnover: string;
   dividend_yield: string;
   inception_date: string;
   leveraged: string;
-  sectors: string[];
-  holdings: {symbol: string, description: string, weight: string}[];
+  sectors: Sector[];
+  holdings: Holding[];
 }
 
 export interface EtfPayload {
@@ -23,6 +24,38 @@ export interface EtfResponse {
   source: 'cache' | 'api';
   usedDemoKey: boolean;
   payload: EtfPayload;
+}
+
+function castAsEtf(input : AlphavantageEtfPayload) : Etf {
+  const etf : Etf = {
+    net_assets: Number(input.net_assets),
+    net_expense_ratio: Number(input.net_expense_ratio),
+    portfolio_turnover: Number(input.portfolio_turnover),
+    dividend_yield: Number(input.dividend_yield),
+    inception_date: input.inception_date,
+    leveraged: input.leveraged,
+    sectors: [],
+    holdings: []
+  };
+
+  for (const s of input.sectors) {
+    let cleanSector : Sector = {
+      sector: s.sector,
+      weight: Number(s.weight)
+    };
+    etf.sectors.push(cleanSector);
+  }
+
+  for (const h of input.holdings) {
+    let cleanHolding : Holding = {
+      ticker: h.ticker,
+      description: h.description,
+      weight: Number(h.weight)
+    };
+    etf.holdings.push(cleanHolding);
+  }
+
+  return etf;
 }
 
 @Injectable({
--- src/intersections/intersections.service.ts
+++ src/intersections/intersections.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import { EtfPayload } from '../alphavantage/alphavantage.service';
 
-interface Holding {
+export interface Holding {
     ticker: string;
     description: string;
     weight: number;
+}
+
+export interface Sector {
+    sector: string;
+    weight: number;
+}
+
+export interface Etf {
+  net_assets: number;
+  net_expense_ratio: number;
+  portfolio_turnover: number;
+  dividend_yield: number;
+  inception_date: string;
+  leveraged: string;
+  sectors: Sector[];
+  holdings: Holding[];
 }
 
 export interface Intersection {
@@ -25,7 +41,10 @@ export class IntersectionService {
         for (const e of etfs) {
             for (const h of e.data.holdings) {
                 const symbol = h.symbol;
-                const exists = this.map.some(intersection => intersection.symbol === symbol);
+                let i = this.map.find(entry => entry.symbol === symbol);
+                if (i) {
+                    i.etfs.push(h)
+                }
             }
         }
         return this.map;
